7d5607668856edf605cfa705eb51162369cb46f9
Juan Milkah <juanmilkah@gmail.com>
2025-06-24 10:20:14 +0300

    add fish config file

---
+++ config.fish
@@ -0,0 +1,33 @@
+set -U fish_greeting
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+fish_config theme choose None
+
+set -x EDITOR helix
+set -x TERM screen-256color
+set -x LANG 'en_US.UTF-8'
+
+set PATH $PATH ~/dotfiles
+set PATH $PATH /usr/local/go/bin:
+set PATH $PATH ~/go/bin
+set PATH $PATH ~/.local/bin
+
+alias x='exit'
+alias c='clear'
+alias g='git'
+alias diff='git diff'
+alias gs='git status'
+alias ca='cargo'
+alias ls='ls -lh --color=auto'
+alias grep='grep --color=auto'
+alias tm='tmux-sessionizer'
+alias open='handlr open'
+alias install='sudo pacman -S --needed'
+alias update='sudo pacman -Syu'
+alias remove='sudo pacman -Rnsc'
+alias unused='sudo pacman -Qtdq'
+alias vim='helix'
+alias copy='xclip -sel clip'
